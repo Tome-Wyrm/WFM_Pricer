6b427a46ae8b58ebcd898485b8b9ea067a8025fa
Tome-Wyrm <146680834+Tome-Wyrm@users.noreply.github.com>
2026-05-26 21:04:44 -0700

    Swapping to premade WFM rust crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 pub mod cli;
-pub mod client;
 pub mod config;
 pub mod decryption;
 pub mod ingestion;
--- src/models.rs
+++ src/models.rs
@@ -146,11 +146,6 @@ pub struct BlacklistConfig {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct WfmStatsResponse {
-    pub payload: WfmStatsPayload,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WfmStatsPayload {
     pub statistics_closed: WfmStatsSubPayload,
     pub statistics_live: WfmStatsSubPayload,
